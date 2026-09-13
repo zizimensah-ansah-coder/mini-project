b3658ca5e5f264abe10e8fbca8deae9780fe4cf0
zizimensah-ansah-coder <zizi. mensah-ansah@amalitechtraining.org>
2026-09-13 09:17:28 +0000

    Displayed the tasks on the page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const taskContainer = document.getElementById("taskContainer");
 // Creating an array of tasks
 let tasks = [
     {
@@ -9,3 +10,16 @@ let tasks = [
         status:"completed"
     }
 ];
+//function to display tasks
+function displayTasks() {
+    tasks.forEach(function(task) {
+        const taskDiv = document.createElement("div");
+        taskDiv.innerHTML =`
+        <p> Title:${task.title}</p>
+        <p>Status:${task.status}</p>
+        `;
+        taskContainer.appendChild(taskDiv);
+    });
+}
+
+displayTasks();
