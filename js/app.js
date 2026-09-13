13b5fe06e79d741a21e611239687acb26a25853a
zizimensah-ansah-coder <zizi. mensah-ansah@amalitechtraining.org>
2026-09-13 08:27:35 +0000

    Added an initial task array

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Creating an array of tasks
+let tasks = [
+    {
+        title:"Go for shopping",
+        status:"pending"
+    },
+    {
+        title:"Do the laundry",
+        status:"completed"
+    }
+];
